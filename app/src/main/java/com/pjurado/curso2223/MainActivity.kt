1df1ef8155a8cbd931f17ddab6c35fc63283887a
pjurado66 <pjurado@ieshnosmachado.org>
2022-09-23 23:49:50 +0200

    Cambio de estilo de un componente

--- app/src/main/java/com/pjurado/curso2223/MainActivity.kt
+++ app/src/main/java/com/pjurado/curso2223/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         val user = findViewById<EditText>(R.id.email)
         val pass = findViewById<EditText>(R.id.password)
         val button = findViewById<Button>(R.id.button)
+
+        supportActionBar?.title = "Hola a todos"
 
         button.setOnClickListener {
             val msg = "Se ha loggeado el usuario ${user.text} con la contraseña ${pass.text}"
