88b356f07ad2f7e1304ba069af19d4e961d072c3
pjurado66 <pjurado@ieshnosmachado.org>
2022-10-02 10:30:19 +0200

    Optimización de código

--- app/src/main/java/com/pjurado/curso2223/DetailFragment.kt
+++ app/src/main/java/com/pjurado/curso2223/DetailFragment.kt
@@ -3,6 +3,7 @@ package com.pjurado.curso2223
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.pjurado.curso2223.databinding.FragmentDetailBinding
@@ -13,6 +14,10 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     companion object{
         const val EXTRA_MOVIE = "DetailActivity:Movie"
 
+        fun create(movie: Movie): DetailFragment =
+            DetailFragment().apply {
+                arguments = bundleOf(EXTRA_MOVIE to movie)
+            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/pjurado/curso2223/MainFragment.kt
+++ app/src/main/java/com/pjurado/curso2223/MainFragment.kt
@@ -1,11 +1,10 @@
 package com.pjurado.curso2223
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.pjurado.curso2223.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
@@ -14,26 +13,22 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentMainBinding.bind(view).apply {
             recycler.adapter = MoviesAdapter(movies){ movie ->
-                navigateto(movie)
+                navigateTo(movie)
             }
         }
         (requireActivity() as AppCompatActivity).supportActionBar?.title = getString(R.string.app_name)
     }
 
-    private fun navigateto(movie: Movie) {
-        val fragment = DetailFragment()
-        fragment.arguments = bundleOf(DetailFragment.EXTRA_MOVIE to movie)
-
-        parentFragmentManager
-            .beginTransaction()
-            .setCustomAnimations(R.anim.slide_right_in,
+    private fun navigateTo(movie: Movie) {
+        parentFragmentManager.commit {
+            setCustomAnimations(R.anim.slide_right_in,
                 R.anim.slide_left_out,
                 R.anim.slide_left_in,
                 R.anim.slide_right_out)
-            .replace(R.id.fragment_container_view,fragment)
-            .setReorderingAllowed(true)
-            .addToBackStack(null)
-            .commit()
+            replace(R.id.fragment_container_view, DetailFragment.create(movie))
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
     }
 }
 
