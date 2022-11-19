b5477f88a29b2c3081d6beaef82ab38a8512d970
pjurado66 <pjurado@ieshnosmachado.org>
2022-11-19 21:28:01 +0100

    Menu AppBar

--- app/src/main/java/com/pjurado/curso2223/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/pjurado/curso2223/ui/detail/DetailFragment.kt
@@ -3,16 +3,21 @@ package com.pjurado.curso2223.ui.detail
 import android.annotation.SuppressLint
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources.getDrawable
-import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.bundleOf
 import androidx.core.text.bold
 import androidx.core.text.buildSpannedString
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.pjurado.curso2223.R
 import com.pjurado.curso2223.databinding.FragmentDetailBinding
 import com.pjurado.curso2223.loadUrl
@@ -33,9 +38,28 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentDetailBinding.bind(view)
 
-        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigate(
+            R.id.action_detailFragment_to_mainFragment
+        ) }
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId){
+                R.id.otro -> {
+                    muestraDialogo(binding, "Otra opción")
+                    true
+                }
+                R.id.help -> {
+                    muestraDialogo(binding, "Ayuda")
+                    true
+                }
+                else -> false
+            }
+        }
+        //(requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
 
         viewModel.movie.observe(viewLifecycleOwner){ movie ->
+            binding.toolbar.title = movie.title
             (requireActivity() as AppCompatActivity).supportActionBar?.title = movie.title
             binding.imagen.loadUrl(movie.urlImagen)
             binding.descripcion.text = movie.description
@@ -43,19 +67,23 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         }
 
         binding.fab.setOnClickListener {
-            val builder = AlertDialog.Builder(requireContext())
-            builder.setMessage("¿Añadir a favoritos?")
-                .setPositiveButton("Ok",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        binding.fab.setImageDrawable(requireContext().getDrawable(R.drawable.ic_baseline_favorite_24))
-                    })
-                .setNegativeButton("Cancel",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // User cancelled the dialog
-                    })
-            builder.create()
-            builder.show()
+            muestraDialogo(binding, "¿Añadir a favoritos?")
         }
+    }
+
+    private fun muestraDialogo(binding: FragmentDetailBinding, mensaje: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage(mensaje)
+            .setPositiveButton("Ok",
+                DialogInterface.OnClickListener { dialog, id ->
+                    binding.fab.setImageDrawable(requireContext().getDrawable(R.drawable.ic_baseline_favorite_24))
+                })
+            .setNegativeButton("Cancel",
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        builder.create()
+        builder.show()
     }
 
     private fun bindingDetail(detalles: TextView, movie: Movie) {
@@ -70,7 +98,5 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             appendLine(movie.valoracion.toString())
         }
     }
-
-
 }
 
